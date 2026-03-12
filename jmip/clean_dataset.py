5bb77f28a95ac189aabf6e1b14080f01d9279482
kadenhoang <hoanggiaanhvu95@gmail.com>
2026-03-11 21:52:55 -0500

    clean the dataset

--- jmip/clean_dataset.py
+++ jmip/clean_dataset.py
@@ -1,6 +1,29 @@
 import pandas as pd
 
-df = pd.read_csv("Uncleaned_DS_jobs.csv")
+df = pd.read_csv("data/Uncleaned_DS_jobs.csv")
 pd.options.display.max_columns= 16
+pd.set_option("display.expand_frame_repr", False)
+pd.set_option("display.width", 200)
+#inspect
+print(df.head())
+print()
 print(df.info())
-print(df.head(10))
+print()
+print(df.describe())
+print()
+print(df.isna().sum())
+print()
+print(df.duplicated().sum())
+
+#drop the unecessary columns
+df_cleaned = df.drop(["index", "Headquarters", "Founded", "Type of ownership", "Sector", "Revenue", "Competitors", "Size"], axis=1)
+
+#clean the unecessary letters
+df_cleaned["Company Name"] = df_cleaned["Company Name"].str.replace(r'[^a-zA-Z]','',regex=True)
+
+#inspect
+print(df_cleaned.head())
+print()
+print(df_cleaned.info())
+
+df.to_csv("data/Cleaned_DS_jobs.csv",index=False)
