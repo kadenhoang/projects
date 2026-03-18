cb73720bc628b2609aac34e8a858816feb7ef022
kadenhoang <hoanggiaanhvu95@gmail.com>
2026-03-18 15:43:42 -0500

    cleaning NIke data

--- jmip/database.py
+++ jmip/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 #create a database
-Database_url = "postgresql://postgres:password@localhost:5432/jobmarket"
+Database_url = "postgresql://hello:1234@localhost:5432/jobmarket"
 
 #engine to connect to the database
 engine = create_engine(Database_url)
@@ -11,5 +11,7 @@ engine = create_engine(Database_url)
 #a workplace to work with the database
 SessionLocal = sessionmaker(bind=engine)
 
-#create a parent class
+#create a parent class - foundation of database models
+#models inherits from Base becomes a mapped table 
 Base = declarative_base()
+
--- pandas/Nike_Sales_Data_Cleaning.py
+++ pandas/Nike_Sales_Data_Cleaning.py
@@ -13,36 +13,53 @@ print(df.isna().sum())
 print()
 print(df.duplicated().sum())
 
-pd.options.display.max_columns=14
+pd.options.display.max_columns=17
+
+
+dfc = pd.DataFrame(df)
+#Cleaning:
+
+# - Order_Date Column
 
 #the Order_Date is 'str' type and has mixed format -> change it to datetime with matching format
-df["Order_Date"] = pd.to_datetime(df["Order_Date"],dayfirst=True, errors = "coerce") #date has mixed format
+dfc["Order_Date"] = pd.to_datetime(dfc["Order_Date"],dayfirst=True, errors = "coerce") #date has mixed format
 #double check
-print(df.info())
-print(df.head(50))
+print(dfc.info())
+
 print()
 
-#Cleaning:
 
 # - Size column:
 
 #The products are shoes but there are clothing sizes in 'Size'
 #check for all the sizes
-print(df["Size"].unique())
+print(dfc["Size"].unique())
 #create a column to show all the rows that missing data
-df["missing_size"] = df["Size"].isna().astype(bool)
+dfc["missing_size"] = dfc["Size"].isna().astype(int)
 #Since there is no numeric kids size, I decide to keep it as NaN
-df.loc[df["Gender_Category"] == "Kids", "Size"] = None
+dfc.loc[dfc["Gender_Category"] == "Kids", "Size"] = None
 #Remove sizes in letter
-df["Size"] = pd.to_numeric(df["Size"], errors= "coerce") 
+dfc["Size"] = pd.to_numeric(dfc["Size"], errors= "coerce") 
 #fill the empty size of each group with median value of each group
-df["Size"] = df.groupby("Gender_Category")["Size"].transform(
+dfc["Size"] = dfc.groupby("Gender_Category")["Size"].transform(
     lambda x: x.fillna(x.median())
 )
 
 
 #df["return_flag"] = (df["Units_Sold"] < 0).astype(int) #flag if a product is returned (negative)
 #df["sale_flag"] = (df["Units_Sold"] > 0).astype(int) # flag if a product is sold
-#df["unsold_flag"] = df["Units_Sold"].isna().astype(int)
-#df["revenue"] = 
-print(df["Size"].head(50))
+
+# - Unit_Sold - MRP - Revenue
+#flag unsold product
+dfc["unsold_flag"] = dfc["Units_Sold"].isna().astype(int)
+#fill the NaN with 0
+dfc["Units_Sold"] = dfc["Units_Sold"].fillna(0)
+
+#calculate the Reveneue
+dfc["final_price"] = dfc["MRP"] * ( 1 - dfc["Discount_Applied"])
+dfc["Revenue"] = dfc["final_price"] * dfc["Units_Sold"]
+
+print(dfc.head(30))
+
+mask = dfc[(dfc["Units_Sold"] > 0) & (dfc["MRP"].isna() | dfc["Discount_Applied"].isna())]
+print(mask)
